d54b86660cd76aa72c8f1fd1d750b499d47677ef
phiphicola <dghmg85@gmail.com>
2023-08-22 11:34:25 +0900

    ㅜㅜ

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,34 +14,41 @@ module.exports = {
         'radio-check': "url('/pub/img/radio-check.svg')",
         'radio-check-on': "url('/pub/img/radio-check-on.svg')",
         'select': "url('/pub/img/icon-onoff.svg')",
-      }
-
+      },
+      height:{
+        '13':'3.125rem',
+      },
+      lineHeight: {
+        '13': '3.125rem',
+      },
+      colors: {
+        'gray-300': '#BCC1C5',
+        'gray-400': '#A4AAB0',
+        'gray-600': '#727A82',
+        'gray-700': '#5B6168',
+        'gray-700': '#44494E',      
+        'gray-900': '#2C3036',
+        'gray-line': '#E9EAEB',      
+        'primary-woori': '#0067AC',
+        'primary-dark': '#090909',
+        'primary-blue': '#3A8DFD',
+        'primary-err': '#EA1917',
+        'light': '#F7F8F9',
+        'dark': '#F0F2F4',
+        'disabled': '#EEF0F2',
+        'white': '#FFFFFF',
+        'transparent' : 'transparent'
+      },
+      fontSize:{
+        'md': '0.938rem',
+      },
+      borderRadius: {
+        '2xlm':'1.25rem',
+      },
     },
     fontFamily: {
-      Pretendard: ["Pretendard"],
-    },
-    colors: {
-      'gray-300': '#BCC1C5',
-      'gray-400': '#A4AAB0',
-      'gray-600': '#727A82',
-      'gray-700': '#5B6168',
-      'gray-900': '#2C3036',
-      'gray-line': '#E9EAEB',      
-      'primary-woori': '#0067AC',
-      'primary-dark': '#090909',
-      'primary-blue': '#3A8DFD',
-      'primary-err': '#EA1917',
-      'light': '#F7F8F9',
-      'dark': '#F0F2F4',
-      'disabled': '#EEF0F2',
-      'white': '#FFFFFF',
-    },
-    backgroundPosition: {
-      'right-1-center':'right 1rem center'
-    },
-    fontSize:{
-      'md': '0.938rem',
-    }
+      NotoSansKR: ["NotoSansKR"],
+    },    
   },
   plugins: [
     // require('@tailwindcss/forms'),
